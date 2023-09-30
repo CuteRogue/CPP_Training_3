c8042426b707d4a88ffe7dd4a353c3617af2d6bf
CuteRogue <andhika.rogue@gmail.com>
2023-09-30 12:30:04 +0700

    init cpp training 3

---
+++ Source/CPP_Training_3/CPP_Training_3GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP_Training_3GameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACPP_Training_3GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_Training_3GameMode();
+};
+
+
+
